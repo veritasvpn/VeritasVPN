30e4fb777d4bfcbcf1be2659764fa879bea110e4
JpGarcia19 <juan@sardius.media>
2026-09-12 14:03:49 -0300

    Prepare 0.2.64 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "cloud.veritasvpn"
         minSdk = 26
         targetSdk = 35
-        versionCode = 49
-        versionName = "0.2.63"
+        versionCode = 50
+        versionName = "0.2.64"
     }
 
     signingConfigs {
